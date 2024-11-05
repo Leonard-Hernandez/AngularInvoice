f7b18870fdb595adbdbc2d739fe673e5d4b52034
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-11-04 23:18:42 -0500

    creando el formulario para crear items

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Invoice } from '../models/invoice';
 import { invoiceData } from '../data/invoice.data';
+import { Item } from '../models/item';
 
 
 @Injectable({
@@ -21,6 +22,14 @@ export class InvoiceService {
     this.invoice.items = this.invoice.items.filter(item => item.id !== id);
     const total = this.calculateTotal()
     return {... this.invoice, total};
+  }
+
+  save(item: Item): Invoice{
+
+    this.invoice.items = [...this.invoice.items, item];
+    const total = this.calculateTotal()
+    return {... this.invoice, total};
+
   }
 
   calculateTotal() {
